5e7906e9d37e8861286b0a6fd89bc2affda825d2
wang-kaopu <Wang-kaopu@outlook.com>
2024-03-28 15:04:17 +0800

    feat: commit http-demo

---
+++ smallGroup/week2/http-demo-4/src/com/wkp/servlet/visitServlet.java
@@ -0,0 +1,20 @@
+package com.wkp.servlet;
+
+import javax.servlet.Servlet;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/visitServlet")
+public class visitServlet extends HttpServlet {
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String username = req.getParameter("username");
+        String password = req.getParameter("password");
+        System.out.println("游客身份登录：" + username + ":" + password);
+    }
+}
