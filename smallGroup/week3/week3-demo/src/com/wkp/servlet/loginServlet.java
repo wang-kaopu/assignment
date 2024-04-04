b74bfbb250fc57fba22d9c2ac6f47dbd49c91ea7
wang-kaopu <Wang-kaopu@outlook.com>
2024-04-04 20:42:41 +0800

    feat: modify week3 small group demo

--- smallGroup/week3/week3-demo/src/com/wkp/servlet/loginServlet.java
+++ smallGroup/week3/week3-demo/src/com/wkp/servlet/loginServlet.java
@@ -14,23 +14,6 @@ import java.util.Map;
 public class loginServlet extends HttpServlet {
     @Override
     protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-//        //设置传值的编码
-//        resp.setContentType("text/html;charset=UTF-8");
-//        req.setCharacterEncoding("utf-8");
-//
-//        //数据流获取信息
-//        StringBuilder sb = new StringBuilder();
-//        BufferedReader reader = req.getReader();
-//        char[] buf = new char[1024];
-//        int len;
-//        while ((len = reader.read(buf)) != -1){
-//            sb.append(buf,0,len);
-//        }
-//
-//        //转json
-//        String str = sb.toString();
-//        System.out.println(str);
-//
         Map<String, String> map = getJSONParams.getJSONParams(req);
         String username = map.get("username");
         String password = map.get("password");
